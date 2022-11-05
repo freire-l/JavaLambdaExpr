35436b3b6694b44adb279d0fe55a342e2a2caa2a
Luis Freire <frebermu9@gmail.com>
2022-11-04 21:09:55 -0500

    WhiteSpace done

--- src/org/luisf/model/JavaLambda.java
+++ src/org/luisf/model/JavaLambda.java
@@ -1,4 +1,14 @@
+//Expresión Lambda que elimine espacios, comas y puntos de una frase y además la devuelva la frase convertida en mayúscula.
+
 package org.luisf.model;
 
+import java.util.function.Function;
+
 public class JavaLambda {
+    public static void main(String[] args) {
+        Function<String, String> cleanLine = line -> line.replaceAll("\\W", "").toUpperCase();
+        String result = cleanLine.apply("This is the last time, for real!!.");
+        System.out.println("result = " + result);
+
+    }
 }
